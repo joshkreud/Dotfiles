7d915266f2777021078a90b4dfd6765e1c8fbea8
Joshua Kreuder <Joshua_Kreuder@outlook.de>
2020-11-01 18:22:52 +0100

    Add vundle installation to SymlinkScript and add Readme

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Joshs Dotfiles
+
+
--- symlink.sh
+++ symlink.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Script to symlink Dotfiles to $home
+# Script to symlink Dotfiles to $home and prepare vim plugins
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -nfs $PWD/.tmux.conf $HOME/.tmux.conf
 ln -nfs $PWD/.bashrc $HOME/.bashrc
 ln -nfs $PWD/.vimrc $HOME/.vimrc
