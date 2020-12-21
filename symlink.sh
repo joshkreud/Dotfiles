750c7d15320063deac29bf29fa01dcc5ec34d846
Joshua Kreuder <Joshua_Kreuder@outlook.de>
2020-12-21 23:02:47 +0100

    fix symlink.sh with other pwd

--- symlink.sh
+++ symlink.sh
@@ -5,10 +5,10 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 echo Cloning TPM for Tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 echo Setting symlinks
-ln -nfs $PWD/.tmux.conf $HOME/.tmux.conf
-ln -nfs $PWD/.bashrc $HOME/.bashrc
-ln -nfs $PWD/.vimrc $HOME/.vimrc
-ln -nfs $PWD/.gitconfig $HOME/.gitconfig
+ln -nfs ./.tmux.conf $HOME/.tmux.conf
+ln -nfs ./.bashrc $HOME/.bashrc
+ln -nfs ./.vimrc $HOME/.vimrc
+ln -nfs ./.gitconfig $HOME/.gitconfig
 
 # If ~/.inputrc doesn't exist yet: First include the original /etc/inputrc
 # so it won't get overriden
