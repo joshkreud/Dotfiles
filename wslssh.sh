de3b5f7e5640ec14be53baa97175445db61cc142
Joshua Kreuder <Joshua_Kreuder@outlook.de>
2021-01-06 21:04:13 +0100

    Fix WslSSH.sh

--- wslssh.sh
+++ wslssh.sh
@@ -1,3 +1,4 @@
-WIN_USER = powershell.exe '$env:UserName'
-cp -r /mnt/c/Users/$WIN_USER/.ssh ~
+#!/bin/bash
+WIN_PATH=$(wslpath "$(wslvar USERPROFILE)")
+cp -r ${WIN_PATH}/.ssh ~
 chmod 600 ~/.ssh/id_rsa
