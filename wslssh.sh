6c23d3241339c3e204e4cc4ee941351a7f65a83a
Joshua Kreuder <Joshua_Kreuder@outlook.de>
2021-09-22 21:17:03 +0200

    Handle multiple key pairs

--- wslssh.sh
+++ wslssh.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 WIN_PATH=$(wslpath "$(wslvar USERPROFILE)")
+echo Copying .ssh from $WIN_PATH
 cp -r ${WIN_PATH}/.ssh ~
 chmod 600 ~/.ssh/id_rsa
+
+PRIVKEYS=$(find ~/.ssh -name "*.pub" | sed -r -e "s~(.*)\.pub~\1~g")
+
+for KEY in $PRIVKEYS
+do
+    echo Fixing permissions on $KEY
+    chmod 600 $KEY
+done
