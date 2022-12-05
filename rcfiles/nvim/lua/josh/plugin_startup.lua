d02eead6323febb2cdb01c0f53f2f1ba26e9f8ff
Joshua Kreuder <joshua.kreuder@wetech.de>
2022-12-05 13:02:18 +0100

    [IMP] Nvim: Wilder config

--- rcfiles/nvim/lua/josh/plugin_startup.lua
+++ rcfiles/nvim/lua/josh/plugin_startup.lua
@@ -1,7 +1,22 @@
 local wilder = require("wilder")
-wilder.set_option('renderer', wilder.popupmenu_renderer({
-  pumblend = 20,
-  highlighter = wilder.basic_highlighter(),
-  left = { ' ', wilder.popupmenu_devicons() },
-  right = { ' ', wilder.popupmenu_scrollbar() },
-}))
+wilder.set_option('renderer', wilder.popupmenu_renderer(
+  wilder.popupmenu_border_theme({
+    border= 'rounded',
+    pumblend = 20,
+    highlighter = wilder.basic_highlighter(),
+    left = { ' ', wilder.popupmenu_devicons() },
+    right = { ' ', wilder.popupmenu_scrollbar() },
+  })
+  )
+)
+wilder.set_option('pipeline', {
+  wilder.branch(
+  wilder.cmdline_pipeline({
+    fuzzy = 1,
+    set_pcre2_pattern = 1,
+  }),
+  wilder.python_search_pipeline({
+    pattern = 'fuzzy',
+  })
+  ),
+})
