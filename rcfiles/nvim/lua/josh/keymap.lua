7cca5a07ec53a4d3846d9bd5cfb9747a7db00c38
Joshua Kreuder <joshua.kreuder@wetech.de>
2022-12-06 13:09:24 +0100

    [IMP] Nvim: Control S save

--- rcfiles/nvim/lua/josh/keymap.lua
+++ rcfiles/nvim/lua/josh/keymap.lua
@@ -13,6 +13,8 @@ vim.g.mapleader = " " -- Leader key Space
 -- Global Remaps
 map('n', "<C-d>","<C-d>zz") -- Center Cursor vert after skip half page
 map('n', "<C-u>","<C-u>zz") -- Center cursor vert after skip half page
+map('n', "<C-s>", "<cmd>w<cr>") -- Control S save
+map('i', "<c-s>", "<ESC><cmd>w<cr>a") -- Control s save in insert mode
 
 -- Fugitive
 map('n', "<leader>gs", "<cmd>Git<CR>")
