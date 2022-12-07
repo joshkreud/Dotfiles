f1c3e5b64d61e2ffae2885f7532f26748aafc99b
Joshua Kreuder <joshua.kreuder@wetech.de>
2022-12-07 13:24:03 +0100

    [FIX] Nvim: Cmp Snippet provider is mandatoty

--- rcfiles/nvim/lua/josh/packer.lua
+++ rcfiles/nvim/lua/josh/packer.lua
@@ -197,12 +197,20 @@ return require('packer').startup(function(use)
       'hrsh7th/cmp-path',
       'hrsh7th/cmp-calc',
       'f3fora/cmp-spell',
-      'hrsh7th/cmp-emoji'
+      'hrsh7th/cmp-emoji',
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip',
     },
     config = function()
       local cmp = require('cmp')
 
       cmp.setup({
+        snippet = {
+          expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+          end,
+        },
+
         mapping = cmp.mapping.preset.insert({
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
